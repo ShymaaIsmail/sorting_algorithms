687e0701afc27fbea1f75bd55c42cbc01001fc87
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-17 09:56:09 +0300

    insert sort in progress

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,13 +10,27 @@ void insertion_sort_list(listint_t **list)
 	if (*list != NULL)
 	{
 		listint_t *current = (*list)->next;
+		listint_t *temp, *prev_temp = NULL;
 
-
-		while (current->next != NULL)
+		while (current != NULL)
 		{
 			printf("%d\n", current->n);
-			if (current->n < current->prev->n)
+			if (current->prev != NULL && current->n < current->prev->n)
 			{
+				temp = current;
+				prev_temp = current->prev;
+
+				prev_temp->next = temp->next;
+				if (temp->next != NULL)
+					temp->next->prev = prev_temp;
+				temp->prev = prev_temp->prev;
+				if (prev_temp->prev != NULL)
+					prev_temp->prev->next = temp;
+				else
+					*list = temp;
+				prev_temp->prev = temp;
+				temp->next = prev_temp;
+				current = prev_temp;
 				print_list(*list);
 			}
 			current = current->next;
