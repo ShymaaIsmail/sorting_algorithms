290c36d16b2f1f201ee1ca445213ce0da599f1e6
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-17 11:31:09 +0300

    task 2 in progress

---
+++ 2-selection_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* selection_sort - insertion_sort_list
+* @array: array of int
+* @size: size
+* Return: void
+*/
+void selection_sort(int *array, size_t size)
+{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+}
